bb0dc386c58000735350918a83a6a6a937a80062
Oziel Alves <oziel.alves.079@ufrn.edu.br>
2021-11-01 16:05:23 -0300

    feat: strapi init and slugify configuration

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a6702c711f1f5fde099b6525ff58c78d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88b83356-9a9f-4f8a-b07a-bcc492e2d8f2'
+};
